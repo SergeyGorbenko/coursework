8688ce5162cc3b29c3727eae1c550a9d1445360b
sergor <sergey13gorbenko@gmail.com>
2018-12-17 12:08:00 +0200

    media

--- gallery/views.py
+++ gallery/views.py
@@ -5,21 +5,25 @@ from gallery.forms import *
 
 def home(request):
     if request.method == 'POST':
-        pass
-    else:
-        gallerys = Gallery_unit.objects.all()
-        photos = []
-        for g in gallerys:
-            photos += [Photo.objects.filter(gallery=g).first()]
-        context = {'gallerys': gallerys, 'photos': photos, 'form': CreateGallery()}
-        return render(request, 'index.html', context=context)
+        form = CreateGallery(request.POST)
+        if form.is_valid():
+            pass
+
+    gallerys = Gallery_unit.objects.all()
+    photos = []
+    for g in gallerys:
+        photos += [Photo.objects.filter(gallery=g).first()]
+    context = {'gallerys': gallerys, 'photos': photos, 'form': CreateGallery()}
+    return render(request, 'index.html', context=context)
 
 
 def gallery(request, gallery):
     if request.method == 'POST':
-        pass
-    else:
-        g = Gallery_unit.objects.get(link=gallery)
-        photos = Photo.objects.filter(gallery=g).all()
-        context = {'gallery': g, 'photos': photos, 'form': LoadPhoto()}
-        return render(request, 'collection.html', context=context)
+        form = LoadPhoto(request.POST)
+        if form.is_valid():
+            pass
+
+    g = Gallery_unit.objects.get(link=gallery)
+    photos = Photo.objects.filter(gallery=g).all()
+    context = {'gallery': g, 'photos': photos, 'form': LoadPhoto()}
+    return render(request, 'collection.html', context=context)
