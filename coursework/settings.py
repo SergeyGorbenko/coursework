59fd24d3b99998282cf9db3bccd812cc37b6f8f2
sergor <sergey13gorbenko@gmail.com>
2018-12-17 10:34:43 +0200

    media

--- coursework/settings.py
+++ coursework/settings.py
@@ -114,10 +114,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
-
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
-parent_dir = os.path.abspath(os.path.dirname(__file__) + '/..')
-MEDIA_ROOT = os.path.join(parent_dir, 'media/')
-MEDIA_URL = 'media/'
